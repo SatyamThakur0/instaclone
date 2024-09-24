fae58562a69796dbe3e8338ba8354cf505256051
Satyam Thakur <sk165344@gmail.com>
2024-09-25 00:44:24 +0530

    s

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -89,8 +89,8 @@ export const loginUser = async (req, res) => {
                 .status(200)
                 .cookie("token", token, {
                     httpOnly: true,
-                    SameSite: "none",
-                    Secure: true,
+                    sameSite: "none",
+                    secure: true,
                     // domain: ".vercel.app",
                 })
                 .json({
