018e31afda0f614beb3a6487c71c448865eb9e15
Satyam Thakur <sk165344@gmail.com>
2024-10-11 00:28:59 +0530

    LL

--- Backend/index.js
+++ Backend/index.js
@@ -26,13 +26,13 @@ const corsOption = {
     origin: "https://instaclonetanx.vercel.app",
     credentials: true,
 };
-app.use(cors(corsOption));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://instaclonetanx.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+app.use(cors(corsOption));
 
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
