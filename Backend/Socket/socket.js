f0c63aed9792695395e61a4435429fa0685a4faa
Satyam Thakur <113540835+SatyamThakur0@users.noreply.github.com>
2024-12-18 14:56:26 +0530

    Update socket.js

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -6,7 +6,7 @@ export const app = express();
 export const server = http.createServer(app);
 export const io = new Server(server, {
     cors: {
-        origin: "https://instaclonetanxapi.vercel.app",
+        origin: "https://instaclonetanx.vercel.app",
         methods: ["GET", "POST"],
     },
 });
